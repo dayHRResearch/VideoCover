949ab648248eb0853d85bd887091e90ced28c452
changyu_liu <changyu_liu@dayhr.com>
2019-07-17 11:05:55 +0800

    Update smile.py

--- source/smile.py
+++ source/smile.py
@@ -1,5 +1,5 @@
-# -*- coding=utf-8 -*-
 import cv2
+import os
 
 # 人脸检测器
 facePath = "./data/lbpcascade_frontalface.xml"
@@ -9,39 +9,49 @@ faceCascade = cv2.CascadeClassifier(facePath)
 smilePath = "./data/haarcascade_smile.xml"
 smileCascade = cv2.CascadeClassifier(smilePath)
 
-img = cv2.imread("../example/test.jpg")
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# 首先检测人脸，返回的是框住人脸的矩形框
-faces = faceCascade.detectMultiScale(
-    gray,
-    scaleFactor=1.1,
-    minNeighbors=8,
-    minSize=(55, 55),
-    flags=cv2.CASCADE_SCALE_IMAGE
-)
+def main():
+    for file in os.listdir('../example/video/'):
+        file_path = os.path.join('../example/video/', file)
+        img = cv2.imread(file_path)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# 画出每一个人脸，提取出人脸所在区域
-for (x, y, w, h) in faces:
-    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
-    roi_gray = gray[y:y + h, x:x + w]
-    roi_color = img[y:y + h, x:x + w]
+        # First, it detects the face and returns a rectangle that holds the face
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=8,
+            minSize=(55, 55),
+            flags=cv2.CASCADE_SCALE_IMAGE
+        )
 
-    # 对人脸进行笑脸检测
-    smile = smileCascade.detectMultiScale(
-        roi_gray,
-        scaleFactor=1.16,
-        minNeighbors=35,
-        minSize=(25, 25),
-        flags=cv2.CASCADE_SCALE_IMAGE
-    )
+        # Draw each face and extract the area of the face
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+            roi_gray = gray[y:y + h, x:x + w]
+            roi_color = img[y:y + h, x:x + w]
 
-    # Frame the corners of the mouth and label the smiling face.
-    for (x2, y2, w2, h2) in smile:
-        cv2.rectangle(roi_color, (x2, y2), (x2 + w2, y2 + h2), (255, 0, 0), 2)
-        cv2.putText(img, 'Smile', (x, y - 7), 3,
-                    1.2, (0, 255, 0), 2, cv2.LINE_AA)
+            # Smiley face detection
+            smile = smileCascade.detectMultiScale(
+                roi_gray,
+                scaleFactor=1.16,
+                minNeighbors=35,
+                minSize=(10, 10),
+                flags=cv2.CASCADE_SCALE_IMAGE
+            )
 
-cv2.imshow('Smile?', img)
-cv2.imwrite("../example/smile.jpg", img)
-c = cv2.waitKey(0)
+            # Frame the corners of the mouth and label the smiling face.
+            for (x2, y2, w2, h2) in smile:
+                cv2.rectangle(
+                    roi_color, (x2, y2), (x2 + w2, y2 + h2), (255, 0, 0), 2)
+                cv2.putText(img, 'Smile', (x, y - 7), 3,
+                            1.2, (0, 255, 0), 2, cv2.LINE_AA)
+
+            if smile is None:
+                continue
+            else:
+                cv2.imwrite("../example/smile.png", img)
+
+
+if __name__ == '__main__':
+    main()
