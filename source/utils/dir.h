8581daca95846241eec08b1f1212476bba6b7c34
Lornatang <shiyipaisizuo@163.com>
2019-07-19 16:47:27 +0800

    Update dir.h

--- source/utils/dir.h
+++ source/utils/dir.h
@@ -5,32 +5,49 @@
 #define MAKE_DIR
 #include <sys/stat.h>
 #include <sys/types.h>
-#endif // MAKE_DIR
+#endif  // MAKE_DIR
+
+#ifndef DIR_OP
+#define DIR_OP
+#include <dirent.h>
+#endif
 
 #ifndef REMOVE_DIR
 #define REMOVE_DIR
 #include <unistd.h>
-#endif // REMOVE_DIR
+#endif  // REMOVE_DIR
 
 #ifndef IO
 #define IO
 #include <iostream>
-#endif // IO
+#endif  // IO
+
+#ifndef VECTOR
+#define VECTOR
+#include <vector>
+#endif
+
+using namespace std;
+
+int checkDirExists();
+int cleanDir();
 
 // Create a directory if the file directory does not exist.
 int checkDirExists() {
   std::string dir_name = "./images";
-  if (access(dir_name.c_str(), 0) != -1)
-    return 0;
-  else {
-    int flag = mkdir(dir_name.c_str(), 0777);
+  if (access(dir_name.c_str(), 0) != -1) {
+    cleanDir();
+    if (mkdir(dir_name.c_str(), 0777))
+      return 0;
+    else
+      return -1;
 
-    if (flag == 0)
+  } else {
+    if (mkdir(dir_name.c_str(), 0777))
       return 0;
     else
       return -1;
   }
-  return 0;
 }
 
 // Delete files generated during program running.
@@ -42,4 +59,4 @@ int cleanDir() {
     return -1;
 }
 
-#endif // UTILS_DIR_H
+#endif  // UTILS_DIR_H
