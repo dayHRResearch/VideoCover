26d03ce60643da892aed475e537acb4eb913b283
Lornatang <shiyipaisizuo@163.com>
2019-07-25 16:39:53 +0800

    Place Copyright Notes

--- include/os.h
+++
@@ -1,4 +0,0 @@
-#ifndef INCLUDE_OS_H
-#define INCLUDE_OS_H
-
-#endif  // INCLUDE_OS_H
--- source/utils/download.cc
+++ source/utils/download.cc
@@ -13,6 +13,7 @@
  * limitations under the License.
  * ==============================================================================
  */
+
 #include "../../include/download.h"
 #include "../../include/log.h"
 
